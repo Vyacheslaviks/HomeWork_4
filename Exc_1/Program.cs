5d64021685c202d36a5c6913dab658ad4140479a
Slava <slavanew98@mail.ru>
2022-12-06 21:25:46 +0300

    1st exercise - completed

--- Exc_1/Program.cs
+++ Exc_1/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход два числа (A и B) и метод который возводит число A в натуральную степень B. 
+//Задание должно быть выполнено в методе. В методе не должно быть вывода в консоль. Не использовать Math.Pow()
+
+//Тело программы
+
+Console.WriteLine("Введите первое число:");
+int A = InputInt();
+Console.WriteLine("Введите второе число: ");
+int B = InputInt();
+
+double result = MyPow(A, B);
+
+Console.WriteLine($"Результат: {result}");
+
+//Методы
+
+int InputInt()
+{
+    bool isParse = int.TryParse(Console.ReadLine(), out int number);
+
+    if (isParse)
+    {
+        return number;
+    }
+    else
+    {
+        Console.WriteLine("Введено некорректное значение!");
+        return -1;
+    }
+}
+
+double MyPow(int A, int B)
+{
+    double result = A;
+    for (int i = 1; i < B; i++)
+    {
+        result = result * A;
+    }
+
+    return result;
+}
