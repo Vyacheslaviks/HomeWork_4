f8d2cc44d0a5cff75d235dbf545c363aeccffb92
Slava <slavanew98@mail.ru>
2022-12-07 18:10:33 +0300

    Created 2nd exercise

---
+++ Exc_2/Program.cs
@@ -0,0 +1,30 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//Тело программы
+
+Console.WriteLine("Введите число: ");
+int number = InputInt();
+
+
+
+//Методы
+
+int InputInt()
+{
+    bool isParse = int.TryParse(Console.ReadLine(), out int number);
+
+    if (isParse)
+    {
+        return number;
+    }
+    else
+    {
+        Console.WriteLine("Введено некорректное значение!");
+        return -1;
+    }
+}
+
+int SumNum(int num)
+{
+    int[] array = new int[]
+}
