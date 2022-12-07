a8994a532aad5b2bb18eedd7dc724096387ef859
Slava <slavanew98@mail.ru>
2022-12-07 20:23:27 +0300

    3rd exercise completed

--- Exc_3/Program.cs
+++ Exc_3/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написать программу, которая задаёт массив из 8ми элементов и выводит его на экран
+
+//Тело программы
+
+OutArray(InputArray());
+
+
+//Методы
+
+int InputInt()
+{
+    bool isParse = int.TryParse(Console.ReadLine(), out int number);
+
+    if (isParse)
+    {
+        return number;
+    }
+    else
+    {
+        Console.WriteLine("Введено некорректное значение!");
+        return -1;
+    }
+}
+
+int[] InputArray()
+{
+    int[] array = new int [8];
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write("Введите число: ");
+        array[i] = InputInt();
+    }
+
+    return array;
+}
+
+void OutArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
